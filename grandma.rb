feb318c2e80bf3e349a4642bde836aa3bd66652f
Christine Xiang <yutingcxiang@gmail.com>
2018-07-03 23:40:00 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -11,3 +11,10 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+
+def speak_to_grandma(string)
+  upper = string.upcase
+  lower = string.downcase
+  
+  
+end
