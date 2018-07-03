959193b330440efa2713e696d78745262d5a2838
Christine Xiang <yutingcxiang@gmail.com>
2018-07-03 23:43:15 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,9 +15,9 @@
 def speak_to_grandma(string)
   upper = string.upcase
   if string == 'I LOVE YOU GRANDMA!'
-    return 'I LOVE YOU TOO PUMPKIN'
-  elsif string = upper 
-    return 'NO, NOT SINCE 1938'
+    return 'I LOVE YOU TOO PUMPKIN!'
+  elsif string == upper 
+    return 'NO, NOT SINCE 1938!'
   else
     return 'HUH?! SPEAK UP, SONNY!'
   end
